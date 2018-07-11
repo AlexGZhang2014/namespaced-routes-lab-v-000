bceca63ad9c7084300eedb357afb41cc9221a4d1
Alex Zhang <alexgzhang92@gmail.com>
2018-07-11 02:08:51 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -26,5 +26,4 @@ class Admin::PreferencesController < ApplicationController
     def pref_params
       params.require(:preference).permit(:song_sort_order, :artist_sort_order, :allow_create_songs, :allow_create_artists, :id)
     end
-  
 end
