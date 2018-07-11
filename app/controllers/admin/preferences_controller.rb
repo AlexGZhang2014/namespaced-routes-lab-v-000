e24355b869fe25e19fe8c0477378bdbaf5132852
Alex Zhang <alexgzhang92@gmail.com>
2018-07-11 01:44:52 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,4 +1,4 @@
 class Admin::PreferencesController < ApplicationController
   def index
-    @preference = Preference.create(song_sort_order: "ASC", artist_sort_order: "ASC", allow_create_song)
+    @preference = Preference.create(song_sort_order: "ASC", artist_sort_order: "ASC", allow_create_songs: true, allow_create_artists: true)
 end
